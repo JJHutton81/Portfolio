7afd16ced03e8fe211d5e1bcb00ac98f376086e4
FreeKeyBoy <jjhutton81@gmail.com>
2019-05-17 11:47:36 -0700

    Changed the fade in on landing page to spring instead of trail, for desired effect.

--- front-end/src/components/landing/LandingPage.js
+++ front-end/src/components/landing/LandingPage.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Trail } from "react-spring/renderprops";
+import { Spring } from "react-spring/renderprops";
 
 import Heart from '../../media/heart.png';
-import HookedComponent from './hooks/Hook1';
 
 import './landing.css';
 
@@ -35,20 +34,53 @@ export default class LandingPange extends Component {
             <div class='element2'>
               <div class='heartWraper'>
                 <div class='landingCard'>
-                  <Trail
-                    items={posts}
-                    keys={post => post.id}
-                    from={{ opacity: 0 }}
-                    to={{ opacity: 1 }}
-                    delay={3000}
-                  >
-                  {post => props => (
-                  <h1 className='landingTitle' style={props} className="post">
-                  {post.title}
-                  </h1>
-                  )}
-                  </Trail>
-                  <div class='buttonWrap'>
+                  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={3000}>
+                  { props => (
+                    <h1 style={ props }>
+                      Hello, I'm James Hutton.
+                    </h1>
+                    ) 
+                  } 
+                  </Spring>
+                  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={5000}>
+                  { props => (
+                    <h1 style={ props }>
+                      I'm a full stack Web Developer.
+                    </h1>
+                    ) 
+                  } 
+                  </Spring>
+                  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={7000}>
+                  { props => (
+                    <h1 style={ props }>
+                      Coding is my Life.
+                    </h1>
+                    ) 
+                  } 
+                  </Spring>
+                  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={9000}>
+                  { props => (
+                    <h1 style={ props }>
+                      UI/UX is my Love.
+                    </h1>
+                    ) 
+                  } 
+                  </Spring>
+                  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={11000}>
+                  { props => (
+                    <div class='buttonWrap' style={ props }>
+                      <Link to='/about'> 
+                        <button
+                          className={(this.state.isActive) ? 'landingButton' : 'fall'}
+                          onClick={this.ToggleClass}>
+                          <p class='pButt'>See my work</p>  
+                        </button>
+                      </Link>
+                    </div>  
+                    ) 
+                  } 
+                  </Spring>
+                  {/* <div class='buttonWrap'>
                     <Link to='/about'> 
                       <button
                         className={(this.state.isActive) ? 'landingButton' : 'fall'}
@@ -56,7 +88,7 @@ export default class LandingPange extends Component {
                         <p class='pButt'>See my work</p>  
                       </button>
                     </Link>
-                  </div>  
+                  </div>   */}
                 </div>
                 <img class='heartBeat' src={Heart} alt='A human heart' />
               </div>
