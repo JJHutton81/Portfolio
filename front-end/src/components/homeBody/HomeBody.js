91992f21c10285c32cfd3f15049e2e9cd966e816
FreeKeyBoy <jjhutton81@gmail.com>
2019-05-17 17:58:47 -0700

    Re-styled projects page.

--- front-end/src/components/homeBody/HomeBody.js
+++ front-end/src/components/homeBody/HomeBody.js
@@ -1,57 +1,13 @@
 import React, { Component } from 'react';
-// import Modal from 'react-modal';
 
 import './homeBody.css';
 
 import STMenu from '../menu/starTrek/STMenu';
 
-// import JOBDThumb from './homeTumbs/jobdthumb.png';
-// import Note from './homeTumbs/note.png';
-
-// Modal.setAppElement('body');
-
-// const customStyles = {
-//   content : {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)'
-//   }
-// };
+import JOBDThumb from './homeTumbs/jobdthumb.png';
+import Note from './homeTumbs/note.png';
 
 export default class HomeBody extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     isOpen: false,
-  //     modalIsOpen: false
-  //   }
-  //   this.openModal = this.openModal.bind(this);
-  //   this.afterOpenModal = this.afterOpenModal.bind(this);
-  //   this.closeModal = this.closeModal.bind(this);
-  // }
-
-  // openModal() {
-  //   this.setState({modalIsOpen: true});
-  // }
-
-  // afterOpenModal() {
-  //   // references are now sync'd and can be accessed.
-  //   this.subtitle.style.color = '#f00';
-  // }
-
-  // closeModal() {
-  //   this.setState({modalIsOpen: false});
-  // }
-
-  // toggleMenu = (e) => {
-  //   this.setState({ isOpen: !this.state.isOpen });
-  // }
-  
-
   render() {
     return (
       <div class='HBWrapper'>
@@ -60,49 +16,17 @@ export default class HomeBody extends Component {
         </div>
         <div class="headerBody">  
           <div class='cardWrapper'>
-          <h1 class="title">James Hutton's Portfolio</h1>
             <div className='cardContainer'>
-            <div class='card' onClick={this.openModal}>
-              <h2>Capstone Project</h2>    
-              <img src={JOBDThumb} alt="Thumbnail of Lab Project" />
+            <div class='card'>
+              <h2 className='CTitle'>Capstone Project</h2>    
+              <img className='JOBD' src={JOBDThumb} alt="Thumbnail of Lab Project" />
             </div>
-            <Modal
-              isOpen={this.state.modalIsOpen}
-              onAfterOpen={this.afterOpenModal}
-              onRequestClose={this.closeModal}
-              style={customStyles}
-              contentLabel="Example Modal"
-            > 
-              <h2 ref={subtitle => this.subtitle = subtitle}>Capstone Project</h2>
-                <img src={JOBDThumb} alt="Thumbnail of Lab Project" />
-                <p>We take the hitch out of getting hitched.</p>
-                <a href="https://joinourbigday.netlify.com" 
-                  rel="noopener noreferrer"
-                  target="_blank"><h2>Go to </h2></a>
-                <h2>About this project</h2>
-                <button onClick={this.closeModal}><h2>Go Back</h2></button>
-            </Modal>
           
             <div class='card' onClick={this.openModal}>
-              <h2>Note Keeping App</h2>    
-              <img src={Note} alt="Thumbnail of Note App" />
+              <h2 className='CTitle'>Note Keeping App</h2>    
+              <img className='JOBD' src={Note} alt="Thumbnail of Note App" />
             </div>
-            <Modal
-              isOpen={this.state.modalIsOpen}
-              onAfterOpen={this.afterOpenModal}
-              onRequestClose={this.closeModal}
-              style={customStyles}
-              contentLabel="Example Modal"
-            > 
-              <h2 ref={subtitle => this.subtitle = subtitle}>Capstone Project</h2>
-                <img src={Note} alt="Thumbnail of Note App" />
-                <p>A basic note keeping app.</p>
-                <a href="https://basicnoteapp.netlify.com" 
-                  rel="noopener noreferrer"
-                  target="_blank"><h2>Go to </h2></a>
-                <h2>About this project</h2>
-                <button onClick={this.closeModal}><h2>Go Back</h2></button>
-            </Modal>
+            
             </div>
           </div> 
         </div>    
