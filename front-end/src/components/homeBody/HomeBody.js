484aef3d4c487e5ecd0a2e0d305f512279c19667
FreeKeyBoy <jjhutton81@gmail.com>
2019-05-22 19:58:30 -0700

    Added large popup to projects page. Restyled. Cleaned up code for formatting.

--- front-end/src/components/homeBody/HomeBody.js
+++ front-end/src/components/homeBody/HomeBody.js
@@ -12,35 +12,35 @@ export default class HomeBody extends Component {
   render() {
     return (
       <Spring config={{duration: 2000}} from={{ marginTop: 1000, marginLeft: 1000 }} to={{ marginTop: 0, marginLeft: 0 }}>
-                    { props => (
-      <div class='HBWrapper' style={ props }>
-        <div>
-          <STMenu />
-        </div>
-        <div class="headerBody">  
-          <div class='cardWrapper'>
-            <div className='cardContainer'>
-              <JOBDPop />
-              <NTAPop />
+        { props => (
+          <div class='HBWrapper' style={ props }>
+            <div>
+              <STMenu />
             </div>
+          <div class="headerBody">  
+            <div class='cardWrapper'>
+              <div className='cardContainer'>
+                <JOBDPop />
+                <NTAPop />
+              </div>
+            </div>
+            <NavLink to='/skills'
+              style={{ 
+                display: 'flex',
+                textDecoration: 'none',
+                alignSelf: 'flex-end',
+                marginRight: '4%',
+              }}
+            > 
+              <button className='ProjectButton'>
+                <p class='pButt'>Next</p>  
+              </button>
+            </NavLink> 
           </div>
-          <NavLink to='/skills'
-            style={{ 
-              display: 'flex',
-              textDecoration: 'none',
-              alignSelf: 'flex-end',
-              marginRight: '4%',
-            }}
-          > 
-          <button className='ProjectButton'>
-            <p class='pButt'>Next</p>  
-          </button>
-          </NavLink> 
-        </div>
         </div>    
-    )
-          }
-          </Spring>  
+      )
+      }
+      </Spring>  
     )
   }
 }
