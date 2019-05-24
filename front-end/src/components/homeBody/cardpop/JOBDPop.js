39b4e068abaa66249088db98ac15680190e89c5c
FreeKeyBoy <jjhutton81@gmail.com>
2019-05-24 15:52:43 -0700

    Finished the third pop up on left side projects card.

--- front-end/src/components/homeBody/cardpop/JOBDPop.js
+++ front-end/src/components/homeBody/cardpop/JOBDPop.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Spring, Transition, animated, config } from "react-spring/renderprops";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-// import JOBDCarousel from './jobdCarousel/JOBDCarousel';
+import JOBDCarousel from './jobdCarousel/JOBDCarousel';
 
 import './jobdpop.css';
 
 import JOBDThumb from '../homeTumbs/jobdthumb.png';
 import NLogo from '../homeTumbs/netlify.png';
 import HLogo from '../homeTumbs/heroku.png';
+import GitHub from '../homeTumbs/github.png';
+import Cat from '../homeTumbs/ghcat.png';
 
 export default class JOBDPop extends Component {
   state = { index: 0 }
@@ -50,17 +52,19 @@ export default class JOBDPop extends Component {
             <h1 className='CTitle'>Join Our Big Day</h1>
             <h2 className='CText'>Join Our Big Day is the result of a five person dev team, over the course of ten weeks. The purpose of the school project was to 
               simulate the real world environment of web development. Each developer worked on both the back-end and the front-end, as needed.</h2>
+            <h1 className='CTitle'>Motivation</h1>
             <h2 className='CText'>Join Our Big Day is an application that solves the problems of managing wedding guests by allowing users to interactively 
               manage and view their guests lists, RSVPs, have access to live upload of photos from all attendees, and use google maps for effortless navigation 
               to get your family and friends where you need them when you need them. We are here to help you get hitched without a hitch.</h2>  
-            <div>
-              <div>  
+            <div className='BigPopMidWrap'>
+              <div className='DepCard'>  
+                <h1 className='CTitle'>Deployment</h1>
                 <h2 className='CText'>The front-end was deployed on</h2>
-                <img src={NLogo} className='NLogo'/>
+                <img src={NLogo} className='NLogo' alt=""/>
                 <h2 className='CText'>The back-end was deployed on</h2>
-                <img src={HLogo} className='HLogo'/>
+                <img src={HLogo} className='HLogo' alt=""/>
               </div>
-              <div>
+              <div className='DevCard'>
                 <h2 className='CTitle'>Developer Team</h2>
                 <h2 className='CText'>James Hutton</h2>
                 <h2 className='CText'>Megan Fontenot</h2>
@@ -68,9 +72,20 @@ export default class JOBDPop extends Component {
                 <h2 className='CText'>Marguel Gutierrez</h2>
                 <h2 className='CText'>Joseph Mercado</h2>
               </div>
-            </div>  
-            <span className='PsudoButt' onClick={this.toggleBack}>Go Back</span>
-            <span className='PsudoButt' onClick={this.toggleClose}>Close</span>
+            </div>
+              <JOBDCarousel />
+              <div className='LinkCard'>
+                <div className='MiniCardBody1'>
+                  <a className='JLink' href="https://github.com/Lambda-School-Labs/labspt2-wedding-website/blob/localFrontend/Frontend/client/README.md" target="_blank" rel="noopener noreferrer"><span className='PsudoButt'>Front-End Readme</span></a>
+                  <a className='JLink' href="https://github.com/Lambda-School-Labs/labspt2-wedding-website/blob/master/Backend/README.md" target="_blank" rel="noopener noreferrer"><span className='PsudoButt'>Back-End Readme</span></a>
+                  <span className='PsudoButt' onClick={this.toggleBack}>Go Back</span>
+                </div>
+                <div className='MiniCardBody2'>
+                  <a className='JLink' href="https://github.com/Lambda-School-Labs/labspt2-wedding-website" target="_blank" rel="noopener noreferrer"><span className='PsudoButt'>Repository on <img className='Cat' src={Cat} alt=""/><img className='GHLogo' src={GitHub} alt=""/></span></a>
+                  <a className='JLink' href="https://joinourbigday.netlify.com" target="_blank" rel="noopener noreferrer"><span className='PsudoButt'>Go To App</span></a>
+                  <span className='PsudoButt' onClick={this.toggleClose}>Close</span>
+                </div>
+              </div>
             </div>
           </animated.div>
         ),
