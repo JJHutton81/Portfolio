a08af78df70d445a9e4a984c67f435da4853e136
FreeKeyBoy <jjhutton81@gmail.com>
2019-06-05 11:41:22 -0700

    Styled the contact form.

--- front-end/src/components/contact/Contact.js
+++ front-end/src/components/contact/Contact.js
@@ -1,62 +1,63 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+import { Spring, Transition, animated, config } from "react-spring/renderprops";
 
 import STMenu from '../menu/starTrek/STMenu';
 
 import './contact.css';
 
-export default class Contact extends Component{
-
-  handleSubmit(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-    axios({
-      method: "POST",
-      url: "http://localhost:3002/send",
-      data: {
-        name: name,
-        email: email,
-        messsage: message
-      }
-    }).then((response) => {
-      if (response.data.msg === 'success') {
-        alert("Message Sent.");
-        this.resetForm()
-      } else if (response.data.msg === 'fail') {
-        alert("Message failed to send.")
-      }
-    })
-  }
-
-  resetForm() {
-    document.getElementById('contact-form').reset();
-  }
-  
+export default class Contact extends Component{  
+  state = { index: 0 }
+    toggleContact = e =>
+      this.setState(state => ({
+        index: state.index === 1 ? 0 : state.index + 1,
+    }))
+    toggleBackContact = e =>
+      this.setState(state => ({
+        index: state.index === 1 ? 0 : state.index - 1,
+    }))
   render(){
+    const pages = [
+      style => (
+        <animated.div style={{ ...style }} className='ContactWrap'>
+          <h1 className='ContactTitle'>This site is under almost constant construction. Check back often for more clever little transitions, animations, and projects. </h1>
+          <h1 className='ContactTitle'>Do you have any questions, comments, or want to work with me?</h1>
+          <span className='PsudoButt' onClick={this.toggleContact}>Contact Me</span>
+          <h1 className='ContactTitle'>Let's make something special.</h1>
+        </animated.div>
+      ),
+      style => (
+        <animated.div style={{ ...style }} className='ContactWrap'>
+          <form className='form-body' method="POST" action="https://formspree.io/jjhutton81@gmail.com">
+            <input type="email" name="email" placeholder="Enter Your Email" className="form-input" />
+            <textarea name="message" contenteditable="true" placeholder="What's on your mind?" wrap="soft" className="form-text"></textarea>
+            <button type="submit" className="Btn">Submit</button>
+            <span className='PsudoButt' onClick={this.toggleBackContact}>Close</span>
+          </form>
+        </animated.div>
+      )
+    ]
     return(
       <div className='ContactBody'>
         <div><STMenu /></div>
-        <div className='ContactWrap'>
-          <h1 className='ContactTitle'>This site is still under construction. Thank you for your patience.</h1>
-          <h1 className='ContactTitle'>Do you have any questions, comments, or want to work with me?</h1>
-          <h1 className='ContactTitle'>Let's make something special.</h1>    
-        {/* </div> */}
-        <form method="POST" action="https://formspree.io/jjhutton81@gmail.com">
-          <div className="form-group">
-            
-            <input type="email" name="email" placeholder="Enter Your Email" className="form-control" />
-          </div>
-          <div className="form-group">
-            
-            <textarea name="message" placeholder="What's on your mind?" className="form-control" rows="5"></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-        </div>
-      </div>
-        )
-    }
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={3000} config={config.molasses}>
+          {props => (
+            <div className='TransitionWrap' style={props}>
+              <Transition
+                native
+                reset
+                unique
+                items={this.state.index}
+                from={{ opacity: 0, transform: 'scale3d(0, 0, 0)' }}
+                enter={{ opacity: 1, transform: 'scale3d(1, 1, 1)' }}
+                leave={{ opacity: 0, transform: 'scale3d(0, 0, 0)' }}>
+                {index => pages[index]}
+              </Transition>
+            </div>
+          )
+          }
+        </Spring> 
+      </div>  
+    )
+  }
 }
 
